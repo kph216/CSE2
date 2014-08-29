2dcdae416f7841c51ff0a19bcc76401520f93a8c
kph216 <kph216@lehigh.edu>
2014-08-29 00:05:26 +0000

    update WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+// Kyle Hughes
+// 8/27
+// hw01
+
+//define class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        //print statement 
+        System.out.println("   -------------"            );
+        System.out.println("   |  WELCOME  |"            );
+        System.out.println("   -------------"            );
+        System.out.println("  ^  ^  ^  ^  ^  ^      "   );
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ "      );
+        System.out.println("|-K--P--H--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /"       );
+        System.out.println("  v  v  v  v  v  v "  );
+        System.out.println(" This August my wife and I took an eleven-day, 675-mile bike");
+        System.out.println(" tour on our tandem. We circled Lake Ontario.");
+ }
+}
